dbfc542e5cfab190f92531931ee00e80698eb507
Neto Cuyabano <cuyabano.neto@gmail.com>
2026-02-28 11:36:35 -0400

    Altera tag para Aluno no bloco 2

--- terraform/s3-static-site/main.tf
+++ terraform/s3-static-site/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket" "site_bucket" {
     Name        = "Bucket para o site estático da Unidade 2"
     Project     = "Especializacao DevOps IFMT"
     ManagedBy   = "Terraform"
-    Professor   = "Waldinei Bispo de Lima"
+    Aluno = "Manoel Felippe F. Cuyabano Neto"
   }
 }
 
